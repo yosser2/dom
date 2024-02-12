ac9d971061284aa9f8842d4d106b2f073020c923
yosser2 <yosserbalti49@gmail.com>
2024-02-12 09:42:27 +0100

    first

---
+++ js.js
@@ -0,0 +1,18 @@
+var chos= document.getElementById("chosen");
+var num = document.getElementById("num");
+var prix = document.getElementById("prix");
+var plus = document.getElementById("plus");
+var moin = document.getElementById("moin");
+var sup = document.getElementById("sup");
+var i3 = document.getElementById("i3");
+var p3 = document.getElementById("p3");
+var i2 = document.getElementById("i2");
+var p2 = document.getElementById("p2");
+var i1 = document.getElementById("i1");
+var p1 = document.getElementById("p1");
+
+
+
+
+
+
